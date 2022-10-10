0209612f3e5437cbc829971e34cd9791f6515b62
Karen Alvarado <112354678+alexalvarad@users.noreply.github.com>
2022-10-10 08:10:26 -0600

    datatable

---
+++ js/recetas.js
@@ -0,0 +1,35 @@
+var crepas = function(){
+
+    Swal.fire({
+        imageUrl: 'imagenes/recetas crepas.jpg',
+        imageAlt: 'A tall image',
+        confirmButtonColor: 'black'   
+    });
+}
+
+var camaron = function(){
+
+    Swal.fire({
+        imageUrl: 'imagenes/camarones.jpg',
+        imageAlt: 'A tall image',
+        confirmButtonColor: 'black'   
+    });
+}
+
+var empanadas = function(){
+
+    Swal.fire({
+        imageUrl: 'imagenes/Recurso 4.jpg',
+        imageAlt: 'A tall image',
+        confirmButtonColor: 'black'   
+    });
+}
+
+var postre = function(){
+
+    Swal.fire({
+        imageUrl: 'imagenes/cake.jpg',
+        imageAlt: 'A tall image',
+        confirmButtonColor: 'black'   
+    });
+}
